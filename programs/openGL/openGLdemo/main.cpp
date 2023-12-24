28e756eed22d4114194eb8140ad75f400b34972b
Russ Koprulu <rustulp@gmail.com>
2023-12-24 07:43:23 -0700

    feat: can draw triangle with GLSL

--- programs/openGL/openGLdemo/main.cpp
+++ programs/openGL/openGLdemo/main.cpp
@@ -1,43 +1,95 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <vector>
-
 #include "Callbacks.h"
 #include "Utilities.h"
 #include "Input.h"
-#include "Vertex.h"
-#include "rendering/OpenGLLoader.h"
-#include "rendering/OpenGLDraw.h"
+#include <iostream>
+#include <random>
+#include <vector>
+#include "rendering/GL/Vertex.h"
+#include "rendering/GL/Draw.h"
+#include "rendering/GL/Version.h"
+#include "rendering/GL/DrawDetails.h"
+#include "rendering/GL/MeshLoader.h"
+#include "rendering/GL/ShaderLoader.h"
+#include "rendering/GL/QueryAttribs.h"
 
 int main(int argc, char* argv[])
 {
 	glfwSetErrorCallback(glfw_error_callback);
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	GLFWwindow* window = glfwCreateWindow(640, 480, extract_version(argv[0]), NULL, NULL);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, extract_version(argv[0]), NULL, NULL);
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	printGLinfo();
+
 	glfwSetWindowCloseCallback(window, glfw_window_close_callback);
 	glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+
+	const char* vertshader =
+		"#version 430                                          \n"
+		"layout(location = 0) in vec3 vertPos;                 \n"
+		"layout(location = 1) in vec3 vertColor;               \n"
+		"layout(location = 0) out vec4 fragColor;               \n"
+		"void main(){                                          \n"
+		"  fragColor = vec4(vertColor, 1.0);                   \n"
+		"  gl_Position = vec4(vertPos, 1.0);                   \n"
+		"}";
+	const char* fragshader =
+		"#version 430                                          \n"
+		"layout(location = 0) in vec4 fragColor;               \n"
+		"out vec4 color;                                       \n"
+		"void main() {                                         \n"
+		"  color = fragColor;                                  \n"
+		"}";
+
+	unsigned int mainShader = LoadShader(vertshader, fragshader);
 	glClearColor(.2f, .2f, .6f, 0.0f);
 
 	std::vector<DrawDetails> drawDetails;
+	{
 
-	std::vector<Vertex> vertices;
-	vertices.emplace_back(0.5f, -0.5f, 0.0f); // bottom left
-	vertices.emplace_back(-0.5f, 0.5f, 0.0f); // bottom right
-	vertices.emplace_back(0.0f, 0.5f, 0.0f); // top
+		const float posData[] = {
+			-0.8f, -0.8f, 0.0f,
+			0.8f, -0.8f, 0.0f,
+			0.0f,  0.8f, 0.0f
+		};
 
-	std::vector<uint32_t> elems = { 0, 1, 2 };
+		const float colorData[] = {
+			1.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 1.0f
+		};
 
-	// Upload vertex data to GPU
-	drawDetails.push_back(UploadMesh(vertices, elems));
+		const GLuint elems[] = { 0, 1, 2 };
+		drawDetails.push_back(UploadMesh(posData, colorData, sizeof(posData)/sizeof(posData[0]),
+			elems, sizeof(elems)/sizeof(elems[0])));
+	}
+
+	//std::default_random_engine generator;
+	//std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+
+	QueryAttribs(drawDetails.back().vao);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		ProcessInput(window);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(mainShader);
+
+		/*float c1 = distribution(generator);
+		float c2 = distribution(generator);
+		float c3 = distribution(generator);*/
+
+		/*uint32_t var = glGetUniformLocation(mainShader, "ucolor");
+
+		glUniform3f(var, c1, c2, c3);*/
+
 		Draw(drawDetails);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
